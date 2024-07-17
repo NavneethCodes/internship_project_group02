65b9e59ef17ab95ad2b263054abc9bb5b122f024
NavneethCodes <navneetharun04@gmail.com>
2024-07-17 23:26:15 +0530

    Users now avaliable in localhost:4000/users by Navneeth

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const cors = require('cors');
-const app = new express();
+const app = express();
 const PORT = 4000;
+require('./connection');
+const userModel = require('./models/userData');
+const eventModel = require('./models/eventData');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+    console.log('inside');
+    try {
+        const data = await userModel.find();
+        console.log(data);
+        res.send(data);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
--- backend/models/userData.js
+++ backend/models/userData.js
@@ -1,12 +1,13 @@
 const { userDbConnection } = require('../connection');
+const db = require('mongoose');
 
-const userSchema = new userDbConnection.Schema({
+const userSchema = new db.Schema({
   userContact: String,
   userEmail: String,
   userName: String,
   userPassword: String,
   userStatus: String,
-  liked_events: [{ type: userDbConnection.Schema.Types.ObjectId, ref: 'events' }]
+  liked_events: [{ type: db.Schema.Types.ObjectId, ref: 'events' }]
 });
 
 const userData = userDbConnection.model('user', userSchema);
