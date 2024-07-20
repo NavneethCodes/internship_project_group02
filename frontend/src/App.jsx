f16adf492a7dc77319af196f9e5cbbabbc7e0405
afra-hakim <aishuafra@gmail.com>
2024-07-20 13:27:52 +0530

    main page with signup and login updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import './App.css'
-import Sign_up from './components/Sign_up.jsx'
 
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Sign_up from './components/Sign_up.jsx';
+import Mainpage from './components/Mainpage';
+import LoginForm from './components/LoginForm';
 
 function App() {
-
   return (
     <>
-          <Sign_up/>
+      <Routes>
+        <Route path='/' element={<Mainpage />} />
+        <Route path='/login' element={<LoginForm />} />
+        <Route path='/signup' element={<Sign_up />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
