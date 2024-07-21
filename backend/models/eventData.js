774e9157c913a4ae3c82a542fd8dab9746b44b86
NavneethCodes <navneetharun04@gmail.com>
2024-07-21 21:51:21 +0530

    Minor changes, made index.js more effective. Added comments to index.js for better understanding

--- backend/models/eventData.js
+++ backend/models/eventData.js
@@ -2,13 +2,14 @@ const { eventDbConnection } = require('../connection');
 const db = require('mongoose');
 
 const eventSchema = new db.Schema({
-    eventName: String,
-    eventDate: Date,
-    startTime: Date,
-    endTime: Date,
-    location: String,
-    description: String,
-    organizer: String
+    eventName   : String,
+    eventDate   : Date,
+    startTime   : Date,
+    endTime     : Date,
+    location    : String,
+    description : String,
+    organizer   : String,
+    category    : String
 })
 
 const eventData = eventDbConnection.model('event', eventSchema);
