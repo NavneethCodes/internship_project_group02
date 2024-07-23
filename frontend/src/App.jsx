dbcad110a468047dfad305192f0582e238622d65
NavneethCodes <navneetharun04@gmail.com>
2024-07-23 22:44:45 +0530

    Bug fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,5 @@
 import './App.css'
 import React from 'react'
-import MainBar from './components/MainBar';
-import Sign_up from './components/Sign_up'
-import Profile from './components/Profile'
-import Eventdetail from './components/Eventdetail'
-import LoginForm from './components/LoginForm'
 
 import { Routes, Route } from 'react-router-dom';
 import Sidebarr from './components/Sidebarr'; 
