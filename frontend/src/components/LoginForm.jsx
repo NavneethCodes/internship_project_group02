e339685e0f9c3d98e1729fa696e79c9e6819008e
Harisankar S <131843490+hxrrrrri@users.noreply.github.com>
2024-07-27 23:02:55 +0530

    Add files via upload

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -3,22 +3,14 @@ import { AiOutlineUser } from "react-icons/ai";
 import { IoLockClosedOutline } from "react-icons/io5";
 import './LoginForm.css';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; 
+import { Link }  from 'react-router-dom'; 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 const LoginForm = () => {
-
-  const handleClick = () => {
-    window.location.href = '/maineventdetails';
-  };
-
-  const handleButtonClick = () => {
-    handleClick();
-    submit();
-  };
-
+  console.log('LoginForm component rendered'); // Add this line
+  const navigate = useNavigate(); 
   const [credentials, setCredentials] = useState({
     'userEmail': "",
     'userPassword': ""
@@ -26,53 +18,63 @@ const LoginForm = () => {
 
   const changelog = (e) => {
     const { name, value } = e.target;
-    setCredentials({ ...credentials, [name]: value });
+    setCredentials({...credentials, [name]: value });
   };
 
   const submit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:4000/login', credentials).then((res) => {
-      toast.success(res.data.message);
-    }).catch((error) => {
-      if (error.response) {
-        toast.error(error.response.data);
-      } else {
-        toast.error('Login failed');
-      }
-      console.log(error);
-    });
+    console.log('Submit function called'); // Add this line
+    axios.post('http://localhost:4000/login', credentials)
+     .then((res) => {
+        console.log('Login successful:', res.data); // Add this line
+        toast.success(res.data.message);
+        localStorage.setItem('sessionToken', res.data.sessionToken);
+        navigate('/maineventdetails');
+      })
+     .catch((error) => {
+        console.log('Login error:', error); // Add this line
+        if (error.response) {
+          toast.error(error.response.data);
+        } else {
+          toast.error('Login failed');
+        }
+      });
+  };
+
+  const handleButtonClick = () => {
+    submit();
   };
 
   return (
     <div className='login-main-bg'>
-    <div className='bg'>
-      <ToastContainer />
-      <form onSubmit={submit}>
-        <div>
-          <h1>Login</h1>
-        </div>
-        <div className='input_box'>
-          <input type='text' placeholder='Email' required name='userEmail' onChange={changelog} />
-          <AiOutlineUser className='icon' />
-        </div>
-        <div className='input_box'>
-          <input type='password' placeholder='Password' required name='userPassword' onChange={changelog} />
-          <IoLockClosedOutline className='icon' />
-        </div>
-        <div className='rem_me'>
-          <label><input type='checkbox' />Remember Me</label>
-          <a href='#'>Forgot Password?</a>
-        </div>
-        <div className='sub_btn'>
-          <button type='submit' onClick={handleButtonClick}>Login</button>
-        </div>
-        <div className='reg_link'>
-          <p>Don't have an account?<Link to='/signup'> Sign up</Link></p>
-        </div>
-      </form>
-    </div>
+      <div className='bg'>
+        <ToastContainer />
+        <form onSubmit={submit}>
+          <div>
+            <h1>Login</h1>
+          </div>
+          <div className='input_box'>
+            <input type='text' placeholder='Email' required name='userEmail' onChange={changelog} />
+            <AiOutlineUser className='icon' />
+          </div>
+          <div className='input_box'>
+            <input type='password' placeholder='Password' required name='userPassword' onChange={changelog} />
+            <IoLockClosedOutline className='icon' />
+          </div>
+          <div className='rem_me'>
+            <label><input type='checkbox' />Remember Me</label>
+            <a href='#'>Forgot Password?</a>
+          </div>
+          <div className='sub_btn'>
+            <button type='submit' onClick={handleButtonClick}>Login</button>
+          </div>
+          <div className='reg_link'>
+            <p>Don't have an account?<Link to='/signup'> Sign up</Link></p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default LoginForm;
+export default LoginForm; 
