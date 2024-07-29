0c30b4e429a61464dbacaab72df7a08340cc8066
hxrrrrri <tikirekfreefire@gmail.com>
2024-07-30 02:00:44 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,11 @@ import Profile from './components/Profile.jsx';
 import SignUp from './components/SignUp.jsx';
 import LoginForm from './components/LoginForm.jsx';
 import MainEventDisplay from './components/MainEventDisplay.jsx';
-import About from './pages/About.jsx';
-import Sidebarr from './components/Sidebarr.jsx';
-import LandingPage from './components/LandingPage.jsx';
-import EventComments from './components/EventComments'; // Import EventComments
 import HomePage from './components/HomePage.jsx';
-import UserProfile from './components/UserProfile.jsx';
 import AdminDashboard from './components/AdminDashboard.jsx';
-import Dummy from './components/Dummy.jsx';
 import EventComments from './components/EventComments';
 import Adminevent from './components/Adminevent.jsx';
- // Import EventComments
+
 
 function App() {
   return (
@@ -26,11 +20,7 @@ function App() {
         <Route path='/signup' element={<SignUp/>}></Route>
         <Route path='/maineventdetails' element={<MainEventDisplay/>}></Route>
         <Route path='/home' element={<HomePage/>}></Route>
-        <Route path='/profile' element={<UserProfile/>}></Route>
         <Route path='/dashboard' element={<AdminDashboard/>}></Route>
-        
-        <Route path='/comments/:eventId' element={<EventComments/>}></Route> {/* New route for comments */}
-        <Route path='/home' element={<About/>}></Route>
         <Route path='/comments/:eventId' element={<EventComments/>}></Route>
         <Route path='/adminevent' element={<Adminevent/>}></Route>
         <Route path='/profile' element={<Profile/>}></Route> {/* New route for comments */}
