55b95ee2eafdb097f03cd8ee4d07f6e77c3f235d
NavneethCodes <navneetharun04@gmail.com>
2024-07-29 23:00:43 +0530

    Error Corrected in edit user

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -25,15 +25,19 @@ const location = useLocation();
 const navigation = useNavigate();
 
 useEffect(() => {
-  axios.get(`https://localhost:4000/user-info/${sessionStorage.getItem('user_id')}`).then(res => {
-    setForm({
-      userName: res.data.userName || "",
-      userPassword: res.data.userPassword || "",
-      userEmail: res.data.userEmail || "",
-      userContact: res.data.userContact || ""
+  axios.get('http://localhost:4000/user-info/6696a1b3fcb930e9f8ab5f07')
+    .then(response => {
+      setForm({
+        userName: response.data.userName || "",
+        userPassword: response.data.userPassword || "",
+        userEmail: response.data.userEmail || "",
+        userContact: response.data.userContact || ""
+      });
     })
-  })
-})
+    .catch(error => {
+      console.error('Error fetching user data:', error);
+    });
+}, []);
 
   return (
     <>
@@ -46,16 +50,16 @@ useEffect(() => {
         autoComplete="off" 
       >
         <Stack spacing={2} direction="column">
-          <h1 style={{color:'green'}}>Profile</h1>
+          <h1 style={{color:'green'}}>Edit Profile</h1>
           <TextField id="Name-input" label="Name" variant="standard" name="userName" value={form.userName} onChange={valueFetch}  />
           <br />
           <TextField id="email-input" label="Email" variant="standard"  name="userEmail"  value={form.userEmail} onChange={valueFetch}/>
           <br />
           <TextField id="contact-input" label="Contact" variant="standard"  name="userContact"  value={form.userContact} onChange={valueFetch}/>
           <br />
-          <TextField id="new-password-input" label="Password" variant="standard"  name="newPassword"  value={form.newPassword} onChange={valueFetch}/>
+          <TextField id="new-password-input" label="New Password" variant="standard"  name="newPassword"  value={form.newPassword} onChange={valueFetch}/>
           <br />
-          <TextField id="con-password-input" label="Password" variant="standard"  name="conPassword"  value={form.conPassword} onChange={valueFetch}/>
+          <TextField id="con-password-input" label="Re enter Password" variant="standard"  name="conPassword"  value={form.conPassword} onChange={valueFetch}/>
           <br />
           <Button variant="contained" color='success' >Save</Button>
         </Stack>
